50fac1c7313192e3addbde25f2462caa97e234a0
huyan <475616043@qq.com>
2016-09-28 12:23:18 +0800

    up

---
+++ wechart-chatting-robot/src/main/java/com/wechat/chatrobot/service/ChatService.java
@@ -0,0 +1,7 @@
+package com.wechat.chatrobot.service;
+
+/**
+ * Created by huyan on 2016/9/28.
+ */
+public interface ChatService {
+}
---
+++ wechart-chatting-robot/src/main/java/com/wechat/chatrobot/service/impl/BaseChatService.java
@@ -0,0 +1,10 @@
+package com.wechat.chatrobot.service.impl;
+
+/**
+ * Created by huyan on 2016/9/28.
+ */
+public class BaseChatService {
+
+
+
+}
